63fc8542e66cbdf4f3f2b32bda0abf5f925cc916
Wesley Brown <nipponkid7@gmail.com>
2019-06-03 17:13:07 -0400

    Initial Commit

---
+++ Check-It/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Check-It
+//
+//  Created by Wesley Brown on 6/3/19.
+//  Copyright © 2019 Allegory. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
