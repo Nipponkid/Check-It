94014e04d6e9901b70698d879a85c4514d7b7ddf
MarellaB <brandonmarella@gmail.com>
2019-06-10 16:29:32 -0400

    Added fake task to display and added method to add new tasks.

--- Check-It/CheckitViewController.swift
+++ Check-It/CheckitViewController.swift
@@ -8,17 +8,43 @@
 
 import Cocoa
 
-class CheckitViewController: NSViewController {
+class CheckitViewController: NSViewController, NSTableViewDataSource,
+                                NSTableViewDelegate {
 
     @IBOutlet weak var button: NSButton!
+    @IBOutlet weak var list: NSTableColumn!
+    @IBOutlet weak var table: NSTableView!
+    
+    var rowCount = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("View Controller Loaded!")
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return rowCount
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier("TaskItemView"), owner: nil) as? NSTableCellView {
+            if let textField = cell.textField {
+                textField.stringValue = "Anything I can think of now"
+            }
+            return cell
+        }
+        return nil
     }
     
     @IBAction func closeProgram(_ sender: Any?) {
         NSApp.terminate(sender)
+    }
+    
+    @IBAction func createTask(_ sender: Any?) {
+        rowCount += 1
+        table.reloadData()
     }
     
 }
