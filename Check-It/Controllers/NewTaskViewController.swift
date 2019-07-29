0fbc6478ff86327022fc73196fdbb8ebd587960a
Wesley Brown <nipponkid7@gmail.com>
2019-07-29 16:26:10 -0400

    Create the New Task Controller and View Hierarchy
    Move everything to their respective folders in the new folder hierarchy.

---
+++ Check-It/Controllers/NewTaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewTaskViewController.swift
+//  Check-It
+//
+//  Created by Wesley Brown on 7/29/19.
+//  Copyright © 2019 Allegory. All rights reserved.
+//
+
+import Cocoa
+
+class NewTaskViewController: NSViewController {
+    
+    @IBOutlet weak var titleTextField: NSTextField!
+    @IBOutlet weak var descriptionTextField: NSTextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
